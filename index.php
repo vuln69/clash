5b251b501c9f2e1e9886501476169845071b9b00
vuln69 <68821686+vuln69@users.noreply.github.com>
2020-07-27 01:26:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,664 @@
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+	<title></title>
+	<meta charset='utf-8'>
+	<meta name="viewport" content="width=device-width,user-scalable=no" />
+	<meta name="format-detection" content="telephone=no"/>
+	<link rel="stylesheet" href="/asset/jquery/jquery-ui.min.css?v=20190615">
+	<script type="text/javascript">
+		window.elex = window.elex || {};
+		window.elex.tag = 0;
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-136395152-1', 'auto');
+		ga('set', 'page', 'activityCenter');
+		ga('send', 'pageview');	
+	</script>
+	<script type="text/javascript" src="/asset/jquery/jquery.min.js?v=20190615"></script>
+	<style type="text/css">
+		html,body,section,article,header,footer,h1,h2,h3,h4,h5,h6{
+			padding:0;
+			margin:0;
+			font-family:arial,sans-serif;
+			font-size:14px;
+		}
+		.clear{
+			clear:both;
+		}
+		section{
+			max-width:640px;
+			margin:0 auto;
+		}
+		span.btn{
+			background-repeat:no-repeat;
+			background-size:contain;			
+		}
+		span.btn1{
+			background-image:url('/activityCenter/asset/image/icon/n/btn1_bg.png');
+		}
+		span.btn2{
+			background-image:url('/activityCenter/asset/image/icon/n/btn2_bg.png');
+		}
+		span.btn3{
+			background-image:url('/activityCenter/asset/image/icon/n/btn3_bg.png');
+		}
+		section.recommand{
+			background:#262019 url('/activityCenter/asset/image/icon/n/item_bg.png') no-repeat 50% bottom;
+		}
+		section.recommand article{
+			background-color:#262019;
+			background-repeat:no-repeat;
+			background-size:contain;
+			background-image:url('/activityCenter/asset/image/icon/n/recommand_bg.png');
+			position:relative;
+		}
+		section.recommand .title{
+			font-size:16px;
+			color:#ffea94;
+			font-weight:bold;
+			text-align:center;
+			height:0;
+			overflow:hidden;
+		}
+		section.recommand .back{
+			position:absolute;
+			left:0;
+			top:0;
+			width:15%;
+			height:15%;
+			z-index:9998;
+			color:transparent;
+			text-indent:-1000px;
+		}
+		section.recommand span.a17{
+			display:none;			
+		}
+		section.recommand .gift,section.recommand .gift2{
+			background-repeat:no-repeat;
+			background-size:contain;
+			position:absolute;
+			right:14%;
+			bottom:34%;
+			z-index:9998;			
+		}
+		section.recommand .gift{
+			background-image:url('/activityCenter/asset/image/icon/n/gift.png');
+			cursor:pointer;
+		}
+		section.recommand .gift2{
+			background-image:url('/activityCenter/asset/image/icon/n/gift2.png');
+		}
+		section.recommand .cnt{
+			position:absolute;
+			left:0;
+			right:0;
+			bottom:16%;
+			text-align:center;
+			z-index:9998;
+		}
+		section.recommand .slogan{
+			color:#f17053;
+		}
+		section.recommand span.btn{
+			display:inline-block;
+			background-repeat:no-repeat;
+			background-size:100%;
+			background-position:50%,50%;
+			color:#fff;
+			width:45%;
+			height:0;
+			font-size:16px;
+			overflow:hidden;
+			font-weight:bold;
+			margin-bottom:-2px;
+		}
+		section.item{
+			background:#262019 url('/activityCenter/asset/image/icon/n/item_bg.png') no-repeat 50% 0;
+		}
+		section.item .col{
+			float:left;
+			width:50%;
+			overflow:hidden;
+		}
+		section.item .thumbnail,section.item .title{
+			background-repeat:no-repeat;
+			background-size:contain;
+			position:relative;
+		}
+		section.item .hot,section.item .new{
+			background-repeat:no-repeat;
+			background-size:contain;
+			background-image:url('/activityCenter/asset/image/icon/n/hot.png?v=20190614');
+			position:absolute;
+			right:10%;
+			top:0;
+			z-index:9900;
+		}
+		section.item .new{
+			background-image:url('/activityCenter/asset/image/icon/n/new.png?v=20190614');
+		}
+		section.item .thumbnail{
+			background-image:url('/activityCenter/asset/image/icon/n/recommand_bg.png');
+			color:#fff;
+			z-index:9700;
+		}
+		section.item .thumbnail span{
+			position:absolute;
+			bottom:0;
+			left:0;
+			line-height:1.5;
+			padding:0 12px;
+			background-color:#000;
+			color:#AF3B2D;
+			font-size:12px;
+		}
+		section.item .abstract{
+			padding:6px 12px;
+			line-height:1.8;
+			color:rgb(135,111,73);
+			background-color:rgba(0,0,0,0.3);
+		}
+		section.item .title{
+			background-position:4px 0;
+			background-image:url('/activityCenter/asset/image/icon/n/title_bg.png');
+			text-align:center;
+			font-size:16px;
+			font-weight:bold;
+			color:rgb(199,185,150);
+			top:2px;
+			z-index:9800;
+			margin-bottom:-6px;
+		}
+		section.item .reward{
+			position:relative;
+		}
+		section.item .reward .inner{
+			margin-left:4%;
+			margin-top:6px;
+		}
+		section.item .reward b{
+			margin-left:3%;
+			display:block;
+			line-height:2;
+			font-weight:normal;
+			color:#cacaca;
+		}
+		section.item .reward i{
+			margin-left:3%;
+			width:20%;
+			display:inline-block;
+			background-size:contain;
+			background-repeat:no-repeat;
+		}
+		section.item .reward i{
+			margin-left:3%;
+			width:20%;
+			display:inline-block;
+			background-size:contain;
+			background-repeat:no-repeat;
+		}
+		i.i1{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i1.png');
+		}
+		i.i2{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i2.png');
+		}
+		i.i3{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i3.png');
+		}
+		i.i4{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i4.png');
+		}
+		i.i5{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i5.png');
+		}
+		i.i6{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i6.png');
+		}
+		i.i7{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i7.png');
+		}
+		i.i8{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i8.png');
+		}
+		i.i9{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i9.png');
+		}
+		i.i10{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i10.png');
+		}
+		i.i11{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i11.png');
+		}
+		i.i12{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i12.png');
+		}
+		i.i13{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i13.png');
+		}
+		i.i14{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i14.png');
+		}
+		i.i15{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i15.png');
+		}
+		i.i16{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i16.png');
+		}
+		i.i17{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i17.png');
+		}
+		i.i18{
+			background-image:url('/activityCenter/asset/image/icon/n/reward/i18.png');
+		}
+		section.item .reward .wrapper{
+			position:absolute;
+			left:0;
+			right:0;
+			bottom:15%;
+			text-align:center;
+		}
+		section.item .reward span.btn{
+			display:inline-block;
+			background-repeat:no-repeat;
+			background-size:100%;
+			background-position:50%,50%;
+			color:#FBD9A7;
+			width:60%;
+			height:0;
+			font-size:16px;
+			overflow:hidden;
+		}
+
+		div.ui-widget-overlay{
+			background:#000;
+			opacity:0.7;
+		}
+
+		.reward-dialog{
+			z-index:9999;
+			background-repeat:no-repeat;
+			background-size:contain;
+			background-position:50% 50%;
+			background-image:url('/activityCenter/asset/image/icon/n/dialog_bg.png?20190621');
+			padding:0;
+			margin:0;
+			border:none!important;
+		}
+
+		.reward-dialog .inner{
+			text-align:center;
+		}
+		.reward-dialog .ui-widget-header{
+			display:none;
+		}
+		.ui-widget-content{
+			color:#fff!important;
+			background-color:transparent!important;
+		}
+		.reward-dialog h5{
+			line-height:1.5;
+			margin:25% 0 10%;
+		}
+		.reward-dialog i{
+			width:30%;
+			margin:0 1.5%;
+			display:inline-block;
+			background-size:contain;
+			background-repeat:no-repeat;
+		}
+		.reward-dialog span.btn{
+			display:inline-block;
+			width:60%;
+			margin-top:10%;
+			cursor:pointer;
+		}
+		.loading{
+			position:fixed;
+			left:0;
+			top:0;
+			right:0;
+			bottom:0;
+			z-index:10000;
+			background:rgba(0,0,0,0.7);
+			display:none;
+			text-align:center;
+			color:#fff;
+			font-weight:bold;
+			font-size:20px;
+		}
+		.loading span{
+			position:relative;
+			top:48%;
+		}
+	</style>
+</head>
+<body>
+
+
+<section class="recommand">
+	<article style="background-image:url('/activityCenter/asset/image/icon/n/recommand2_bg.png'),url('/activityCenter/asset/image/1595405868.jpg')">
+		<div class="title">Benteng Dominasi Dunia</div>
+		<span class="gift2"></span>
+		<a class="back" href="ecgWebviewClose://webViewAdpter?">&nbsp;</a>
+		<div class="cnt">
+			<a href="/a41/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn2">Join now</span></a>
+			<div class="slogan">20vs20 tim nasional berpartisipasi.</div>
+		</div>
+	</article>
+</section>
+
+
+
+<section class="item">
+	<div class="title">
+		Ulang Tahun ke-6 COK<span class="hot"></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1594117261.jpg')">
+		<span>Participants: 228083</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i16"></i><i class="i17"></i><i class="i18 ii"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/a39/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Acara peringatan dini, bergabung untuk mengumpulkan bingkai  peringatan dan kejutan besar lainnya!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Kuis Piala Dunia S3 CoK<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1583223626.jpg')">
+		<span>Participants: 181644</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i2"></i><i class="i5"></i><i class="i6 ii"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/a31/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Mari menjadi saksi di final!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		S3 World Cup Road to Championship<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1592820364.jpg')">
+		<span>Participants: 112474</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			
+		</div>
+		<div class="wrapper">
+			<a href="/a30/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Fair competition of strategy and teamwork
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Piala Dunia Internasional<span class="hot"></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1590656628.jpg')">
+		<span>Participants: 289093</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i1"></i><i class="i2"></i><i class="i3 ii"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/a37/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Piala Dunia Internasional, hadiah terus berdatangan!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Knight's Round Table<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1561628706.jpg')">
+		<span>Participants: 0</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i6"></i><i class="i10"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/x/c.php?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Host a meet-up in your city! Make new friends, share your strategies, have a great time!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Hall of Fame S7<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1575527541.jpg')">
+		<span>Participants: 423548</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i2"></i><i class="i3"></i><i class="i7 ii"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/a25/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		We hope that players from all over the world could get to know each other.
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Strategy Site<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1563521524.jpg')">
+		<span>Participants: 365422</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			
+		</div>
+		<div class="wrapper">
+			<a href="/a19/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		We want to build a platform that contains various strategies and hope it will be helpful!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Gift code exchange center<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1557226604.jpg')">
+		<span>Participants: 87534</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			
+		</div>
+		<div class="wrapper">
+			<a href="/a12/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Gift code exchange center
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Points store<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1594798159.jpg')">
+		<span>Participants: 557070</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i6"></i><i class="i10"></i><i class="i11 ii"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/store/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Signing in, staying online and recharging can earn you points for redeeming rare items. Stay tuned!
+	</div>
+</section>
+
+
+<section class="item">
+	<div class="title">
+		Sign-in Milestone<span class=""></span>
+	</div>		
+	<div class="col thumbnail" style="background-image:url('/activityCenter/asset/image/1561628807.jpg')">
+		<span>Participants: 1146862</span>
+	</div>
+	<div class="col reward">
+		<div class="inner">
+			<b>Reward</b>
+			<i class="i15"></i>
+		</div>
+		<div class="wrapper">
+			<a href="/a00/?uid=1812639737002093&regCountry=ID&lang=id&kingdom=1"><span class="btn btn1">Join now</span></a>
+		</div>
+	</div>
+	<div class="clear abstract">
+		Accumulate sign-in days to collect a precious nameplate, sign-in to unlock achievements and stamps
+	</div>
+</section>
+
+
+	<script type="text/javascript">
+		(function(){
+			var btnHeight = parseInt($("section.recommand .btn").width()*83/327);
+			var articleHeight = parseInt($("section").width()*800/1080);
+			var titleHeight = parseInt(articleHeight*0.12);
+			var giftHeight = parseInt($("section").width()*0.16);
+			$("section.recommand article").css("height",articleHeight);
+			$("section.recommand .title").css({"height":titleHeight,"line-height":titleHeight+"px"});
+			$("section.recommand .btn").css({"height":btnHeight,"line-height":(btnHeight*0.8)+"px"});
+			$("section.recommand .gift").css({"height":giftHeight,"width":giftHeight+1});
+			$("section.recommand .gift2").css({"height":giftHeight,"width":giftHeight+1});
+		})();
+	</script>
+	<script type="text/javascript">
+		(function(){
+			var colHeight = parseInt($("section.item .col").width()*758/1080);
+			var rewardHeight = parseInt($("section.item i").width());
+			var titleHeight = parseInt($("section").width()*91/1080);
+			var btnHeight = parseInt($("section.item .btn").width()*72/264);
+			$("section.item .col").css("height",colHeight);
+			$("section.item i").css("height",rewardHeight);
+			$("section.item .title").css({"height":titleHeight,"line-height":titleHeight+"px"});
+			$("section.item .hot").css({"height":titleHeight*1.2,"width":titleHeight*1.2});
+			$("section.item .new").css({"height":titleHeight*1.2,"width":titleHeight*1.2});
+			$("section.item .btn").css({"height":btnHeight,"line-height":btnHeight+"px"});
+		})();
+	</script>
+	<div id="dialog" class="dialog" title="Reward">
+		<div class="inner">
+			<h5>Collected successfully, please check your in-game mail!</h5>
+			<span class="btn btn3">OK</span>
+		</div>
+	</div>
+	<div class="loading"><span>loading...</span></div>
+	<script src="/asset/jquery/jquery-ui.min.js?v=20190615"></script>
+	<script type="text/javascript">
+		$(function(){
+			var width = parseInt($("section").width()*0.55);
+			var height = parseInt(width*316/300);
+			$("#dialog").dialog({
+				autoOpen:true,
+				modal:true,
+				width:width,
+				height:height,
+				dialogClass: "reward-dialog",
+				classes:{
+					"ui-dialog":""
+				}
+			});
+			$("#dialog span.btn").click(function(){
+				$("#dialog").dialog("close");
+			});
+
+			var padding = (width*0.12)+"px";
+			$(".reward-dialog .inner").css({"padding":(width*0.1)+"px",height:width*0.77});
+			$(".reward-dialog i").css("height",$(".reward-dialog i").width());
+			var btnHeight = parseInt($(".reward-dialog .btn").width()*72/264);
+			$(".reward-dialog .btn").css({"height":btnHeight,"line-height":btnHeight+"px"});
+			$("#dialog").dialog("close");
+
+			$("section.recommand").on("click","span.gift",function(){
+				window.elex.tag = 1;
+				$("#dialog").dialog("open");
+				$(this).removeClass("gift").addClass("gift2");
+				$.ajax({url:"/activityCenter/api.php?a=2&uid=1812639737002093&lang=id"});
+				
+			});
+
+			$("body").on("click","div.ui-widget-overlay",function(){
+				$("#dialog").dialog("close");
+				$.ajax({url:"/activityCenter/api.php?a=2&uid=1812639737002093&lang=id"});
+			});
+
+			$("a").click(function(){
+				$(".loading").css("display","block");
+			});
+			$(window).on("pageshow",function(){
+				$(".loading").css("display","none");
+			});
+
+		});
+
+
+	</script>
+</body>
+</html>
